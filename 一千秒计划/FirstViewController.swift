b708c03f9e1a1ff487418dc2c18f2483310f76a1
mrwang90hou <mrwang90hou@163.com>
2018-01-30 17:18:21 +0800

    sen3 commit

---
+++ 一千秒计划/FirstViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FirstViewController.swift
+//  一千秒计划
+//
+//  Created by 王宁 on 2017/3/28.
+//  Copyright © 2017年 mrwang90hou. All rights reserved.
+//
+
+import Foundation
---
+++ 一千秒计划/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  一千秒计划
+//
+//  Created by 王宁 on 2017/3/28.
+//  Copyright © 2017年 mrwang90hou. All rights reserved.
+//
+
+import UIKit
+class UITabBarControllerController:UIViewController{
+    @IBOutlet var tabBarCt1: UIViewController!
+}
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
